420f19d3dc99c98cdb457548a2192376964f7f6c
adilbekm <adilbekm@yahoo.com>
2019-05-15 17:05:23 -0400

    add c code

--- readme.md
+++ readme.md
@@ -1,20 +1,22 @@
 # Sleep Box #
 
-![image of sleep box][img1] ![image of sleep box upside down][img2]
+![Sleep box][img1] ![Sleep box upside down][img2]
 
 This is a version of **Sleep Box** as invented and posted by [Mark Zuckerberg](https://www.facebook.com/zuck) on April 27, 2019. 
 
 ## How Does It Work? ##
 
-This sleep box works just like Mark described it in his post (with a couple additional functions I added myself). The box emits a faint light at a time that you designate, and stays lit for 1 hour. For example, if you set it to 6am, it activates at that time and stays lit until 7am.
+This sleep box works the same way Mark described it in his post, with a couple additional functions I added myself. The sleep box emits a faint light at a time that you designate and stays lit for 1 hour. For example, if you set it to 6am, it activates at that time and stays lit until 7am.
 
-When it is activated, it starts at a tiny intensity at first and takes a few minutes to reach a stable intensity. If you are sensative to light or sleep in a very dark room, it might be nice that the sleep box lights up smoothly.
+When it is activated, it lights up smoothly starting at tiny intensity and reaching stable intensity a few minutes later. This should be helpful if you are sensative to light or sleep in a very dark room.
 
 There is also a demo mode. Once you activate the demo mode, the sleep box lights up and down for about a minute. This is useful if you want to see what the sleep box looks like when it is lit.
 
 ## How Do I Use It? ##
 
-There is only 1 button. You can see it when you remove the outer box. 
+There is only 1 button. You can see it when you remove the outer box: 
+
+![Sleep box with outer box removed][img3]
 
 You press this button as many times as the number of hours from now that you want the sleep box to activate. For example, if it's 10pm now and you want the sleep box to activate at 6am, then you press the button 8 times. When done pressing, the box will blink as many times as you pressed it so you can be sure it was set as you needed. Once set, the sleep box will activate at the set time every day, until you reset it or until the battery dies.
 
@@ -38,21 +40,31 @@ Follow these steps if you would like to make this sleep box yourself.
 2. Solid oak board 4" x 3' ($10)
 3. Four LEDs ($5)
 4. Push button ($2)
-5. 9V battery ($2)
+5. 9V battery ($3)
 
-You will also need some wires, glue, and tools, like a soldering iron and sand paper.
+You will also need some wires, glue, and tools like a soldering iron and sand paper.
 
 ### 2. Make the Outer Structure ###
 
-(coming soon)
+Making the outer box is straightforward. It's made of a top piece and 4 side pieces glued together as follows:
+
+![Outer structure][img4]
 
 ### 3. Make the Inner Structure ###
 
-(coming soon)
+The inner structure is made of 7 pieces glued together as follows:
+
+![Inner structure][img5]
+
+I did not mention the center piece in **Get the Parts**. It can be made of any scrap pieace of wood (oak will work great) that you might have, but if you don't then include this in your shopping list.
 
 ### 4. Assemble the Electronics ###
 
-(coming soon)
+Connect the electronics as follows:
+
+![Circuit diagram][img6]
+
+Then attach everything to the inner structure as shown in a previous picture. You will need to glue the LEDs to the center piece or use a sticky material like I did. The battery and the arduino should fit tightly into the lower compartment without a glue. 
 
 ### 5. Upload the Software ###
 
@@ -60,5 +72,7 @@ Upload the code from "sketch_sleepbox.c" into the Arduino unit by connecting the
 
 [img1]: images/img_01s.jpg
 [img2]: images/img_02s.jpg
-[img3]:
-[img4]:
+[img3]: images/img_03s.jpg
+[img4]: images/img_07.png
+[img5]: images/img_04s.png
+[img6]: images/img_05s.png
---
+++ sketch_sleepbox.c
@@ -0,0 +1,149 @@
+/*
+A Mark Zuckerberg Sleep Box
+*/
+
+int newPress = 0;
+int pressCount = 0;
+int pressCountFinal = 0;
+int counter = 0;
+int counterAlarm = 0;
+int minsLeft = 0;
+int alarmActive = 0;
+int activeCounter = 0;
+int minsLeftActive = 0;
+int outPinLEDs = 5; // use pin #5 for LEDs
+
+void flash(int n, int m) {
+  /* flash LEDs n number of times pausing for m msecs between flashes */ 
+  for (int i=0; i<n; i++) {
+    analogWrite(outPinLEDs, 0);
+    delay(m);
+    analogWrite(outPinLEDs, 70);
+    delay(m);
+  }
+  analogWrite(outPinLEDs, 0);
+  delay(m); 
+}
+
+void doDemo() {  
+  /* light the box on/off 10 times with fade effect */
+  for (int demo=0; demo<10; demo++) {
+    for (int power = 0; power<50; power++) {
+      analogWrite(outPinLEDs, power);
+      int sensorVal = digitalRead(2);
+      if (sensorVal==LOW) { // button pressed
+        goto end;
+      }
+      delay(50);
+    }
+    for (int power=50; power>=0; power--) {
+      analogWrite(outPinLEDs, power);
+      int sensorVal = digitalRead(2);
+      if (sensorVal==LOW) { // button pressed
+        goto end;
+      }
+      delay(50);
+    }
+    for (int i=0; i<10; i++) {
+      int sensorVal = digitalRead(2);
+      if (sensorVal==LOW) { // button pressed
+        goto end;
+      }
+      delay(100);
+    }
+  }
+  end:
+    analogWrite(outPinLEDs, 0);
+    delay(50);
+}
+
+void setup() { 
+  // start serial connection for debugging (comment out when in live mode)
+  // Serial.begin(9600);
+  pinMode(2, INPUT_PULLUP); // pin #2 as input with internal pull-up resistor
+  pinMode(13, OUTPUT); // pin #13 for pushbutton press 
+  pinMode(outPinLEDs, OUTPUT); // pin for LEDs (defined earlier)
+  flash(1, 700); // flash once to indicate successful power-up
+}
+
+void loop() {  
+  // read pushbutton value into variable
+  int sensorVal = digitalRead(2);
+  // Keep in mind the pull-up means the pushbutton's logic is inverted. It goes
+  // HIGH when it's open, and LOW when it's pressed. Turn on pin #13 when the
+  // button's pressed, and off when it's not:
+  if (sensorVal==HIGH) { // button unpressed
+    digitalWrite(13, LOW);
+    newPress = 0;
+  } else { // button pressed
+    digitalWrite(13, HIGH);  
+    if (newPress==0) {
+      newPress = 1;
+      analogWrite(outPinLEDs, 0);
+      alarmActive = 0;
+      activeCounter = 0;
+      minsLeftActive = 0;
+      // if hasn't been long since last press (about 4s or less), 
+      // then increment pressCount
+      if (counter<=50) {
+        pressCount++;
+        counter = 1;
+      }
+    }
+  }
+  if (counter>0 && counter<=50) { // not done pressing, keep listening
+    counter++;
+  } else if (counter>50) { // done pressing, start alarm clock at next loop
+    // Serial.println(pressCount);
+    newPress = 0;
+    pressCountFinal = pressCount;
+    minsLeft = pressCountFinal * 60;
+    counterAlarm = 0;
+    counter = 0;
+    pressCount = 0;
+    // flash as many times as number of button presses to confirm input
+    flash(pressCountFinal, 300);
+  } else if (pressCountFinal==1) { // only 1 press, so demo mode
+    pressCountFinal = 0;
+    doDemo();
+  } else if (pressCountFinal>1) { // more than 1 press, so alarm clock mode
+    counterAlarm++;
+    if (counterAlarm==600) {
+      counterAlarm = 0;
+      minsLeft--;
+      if (minsLeft==0) {
+        // activate alarm for 60 mins, i.e. light the box
+        minsLeft=24*60; // reset to 24 hours
+        alarmActive = 1;
+        minsLeftActive = 60;
+        analogWrite(outPinLEDs, 5); // light with tiny power
+      }
+    }
+  }
+  if (alarmActive==1) { // alarm is active, i.e. box should be lit
+    if (minsLeftActive>0) {
+      if (activeCounter==600) {
+        activeCounter = 0;
+        minsLeftActive--;
+        if (minsLeftActive==59) {
+          analogWrite(outPinLEDs, 10); // send more power
+        } else if (minsLeftActive==58) {
+          analogWrite(outPinLEDs, 15); // send more power
+        } else if (minsLeftActive==57) {
+          analogWrite(outPinLEDs, 20); // send more power
+        } else {
+          analogWrite(outPinLEDs, 50); // full power (set to smaller value if too bright)
+        }
+      } else {
+        activeCounter++;
+      }
+    } else {
+      alarmActive = 0;
+      activeCounter = 0;
+      minsLeftActive = 0;
+      analogWrite(outPinLEDs, 0);
+    }
+  }
+  // delay each loop by 100 ms, so 10 loops = 1 sec
+  delay(100);
+}
